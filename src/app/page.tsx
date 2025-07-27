02afd98b5871493868dc6b2d0cdc4896e5e44ab9
Yash Bhidawe <ybh5179@gmail.com>
2025-07-27 15:50:22 +0530

    remove pulled files

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-
-export default function Home() {
-  return (
-    <div className="flex justify-center items-center h-screen">
-      <h3 className="font-bold text-3xl">Hello World!</h3>
-    </div>
-  );
-}
