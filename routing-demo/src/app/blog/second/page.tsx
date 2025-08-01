e26fd1ef0fd4d0c879fad0ad3d6c44e98c0e9a35
Yash Bhidawe <ybh5179@gmail.com>
2025-08-01 18:03:20 +0530

    loading

--- routing-demo/src/app/blog/second/page.tsx
+++ routing-demo/src/app/blog/second/page.tsx
@@ -1,6 +1,12 @@
 import React from "react";
 
-const Second = () => {
+const Second = async () => {
+  await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("Intentional Delay");
+      console.log("delay");
+    }, 2000);
+  });
   return <div>Second</div>;
 };
 
