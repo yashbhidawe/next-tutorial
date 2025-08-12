cee542e9a37e73579824ac51ea7f3092457ccf9d
Yash Bhidawe <ybh5179@gmail.com>
2025-08-12 09:39:09 +0530

    learn: route handlers, headers, cookies and cashing

--- route-handlers-demo/src/app/profile/api/route.ts
+++ route-handlers-demo/src/app/profile/api/route.ts
@@ -1,3 +1,17 @@
-export async function GET() {
-  return new Response("Profile!");
+import { type NextRequest } from "next/server";
+import { headers } from "next/headers";
+export async function GET(request: NextRequest) {
+  // const requestHeaders = new Headers(request.headers);
+  // console.log(requestHeaders.get("Authorization"));
+
+  const headerList = await headers();
+  console.log(headerList.get("Authorization"));
+  const theme = request.cookies.get("theme");
+  console.log("Theme cookie:", theme);
+  return new Response("<h1>Profile API</h1>", {
+    headers: {
+      "Content-Type": "text/html",
+      "Set-Cookie": "theme=dark",
+    },
+  });
 }
